8eac93ce0c5d0b8840f7a6d0bf258f97e841666d
Ming Hong Pi <12989135+minghongpi@users.noreply.github.com>
2024-12-12 17:27:00 -0800

    Create twoSum.cpp
    leetcode two Sum

---
+++ twoSum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> m;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (m.find(nums[i]) != m.end())
+            {
+                return { i, m[nums[i]] };
+            }
+            m[target - nums[i]] = i;
+        }
+        return { -1, -1 };
+    }
+};
